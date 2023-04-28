6144cd4e515e1c4739f6d8073f0671dcf90fb74b
GiddsRiley <gideon00079@gmail.com>
2023-04-28 02:19:47 -0700

    compiled

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,7 +12,7 @@ size_t print_list(const list_t *h)
 
 	while (h)
 	{
-		printf("[%lu] %s\n", h->len, h->str ? h->str : "(nil)");
+		printf("[%u] %s\n", h->len, h->str ? h->str : "(nil)");
 		h = h->next;
 		nelem++;
 	}
