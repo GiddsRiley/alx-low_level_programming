2e90e4ce71c20012b464ff09c40b613283f7f299
GiddsRiley <gideon00079@gmail.com>
2023-05-04 14:49:32 -0700

    update on function

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,20 +12,20 @@ unsigned int binary_to_uint(const char *b)
 	int len;
 
 	if (b == NULL)
-	return (0);
+		return (0);
 
-	for (int i = 0; b[i] != '\0'; i++)
-{
-	if (b[i] != '0' && b[i] != '1')
-	return (0);
+	for (len = 0; b[len]; len++)
+	{
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
 	}
 
 
-	for (power = 1, sum = 0, len--; len >= 0; len--, power *= 2)
+	for (power = 1, total = 0, len--; len >= 0; len--, power *= 2)
 	{
 		if (b[len] == '1')
-			sum += power;
+			total += power;
 	}
 
-	return (sum);
+	return (total);
 }
